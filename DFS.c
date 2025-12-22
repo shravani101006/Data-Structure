3e9f2dad54446664cc7d61da70104cd8c567e5a6
shravani101006 <shravaniaparaj.cs24@bmsce.ac.in>
2025-12-22 09:25:23 +0530

    Add files via upload

---
+++ DFS.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#define MAX 10
+
+int visited[MAX];
+int adj[MAX][MAX];
+int n;
+
+void DFS(int v){
+    visited[v]=1;
+    printf("%d ",v);
+    for(int i=0;i<n;i++){
+        if(adj[v][i]==1 && !visited[i]){
+            DFS(i);
+        }
+    }
+}
+
+int main(){
+    printf("Enter number of vertices: ");
+    scanf("%d",&n);
+    printf("Enter adjacency matrix:\n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&adj[i][j]);
+        }
+    }
+    for(int i=0;i<n;i++)
+        visited[i]=0;
+    printf("DFS Traversal starting from vertex 0:\n");
+    DFS(0);
+    for(int i=0;i<n;i++){
+        if(!visited[i]){
+            printf("Graph is NOT connected\n");
+            return 0;
+        }
+    }
+    printf("Graph is connected\n");
+    return 0;
+}
